b70e7411045c149072949292dbc031f65df515ac
Francklin <francklinjunior@outlook.com>
2023-07-16 20:43:03 -0300

    Upload das Capas dos Cursos

--- src/adminjs/resources/course.ts
+++ src/adminjs/resources/course.ts
@@ -1,11 +1,51 @@
 // src/adminjs/resources/course.ts
 
-import { ResourceOptions } from "adminjs";
+import { FeatureType, ResourceOptions } from "adminjs";
+import uploadFileFeature from "@adminjs/upload/types/features/upload-file/upload-file.feature";
+import path from "path";
 
 export const courseResourceOptions: ResourceOptions = {
-  navigation: 'Catálogo',
-  editProperties: ['name', 'synopsis', 'uploadThumbnail', 'featured', 'categoryId'],
-  filterProperties: ['name', 'synopsis', 'featured', 'categoryId', 'createdAt', 'updatedAt'],
-  listProperties: ['id', 'name', 'featured', 'categoryId'],
-  showProperties: ['id', 'name', 'synopsis', 'featured', 'thumbnailUrl', 'categoryId', 'createdAt', 'updatedAt']
-}
+  navigation: "Catálogo",
+  editProperties: [
+    "name",
+    "synopsis",
+    "uploadThumbnail",
+    "featured",
+    "categoryId",
+  ],
+  filterProperties: [
+    "name",
+    "synopsis",
+    "featured",
+    "categoryId",
+    "createdAt",
+    "updatedAt",
+  ],
+  listProperties: ["id", "name", "featured", "categoryId"],
+  showProperties: [
+    "id",
+    "name",
+    "synopsis",
+    "featured",
+    "thumbnailUrl",
+    "categoryId",
+    "createdAt",
+    "updatedAt",
+  ],
+};
+
+export const courseResourceFeatures: FeatureType[] = [
+  uploadFileFeature({
+    provider: {
+      local: {
+        bucket: path.join(__dirname, "../../../public"),
+      },
+    },
+    properties: {
+      key: "thumbnailUrl",
+      file: "uploadThumbnail",
+    },
+    uploadPath: (record, filename) =>
+      `thumbnails/course-${record.get("id")}/${filename}`,
+  }),
+];
--- src/adminjs/resources/index.ts
+++ src/adminjs/resources/index.ts
@@ -3,17 +3,19 @@
 import { ResourceWithOptions } from "adminjs";
 import { Category, Course, Episode } from "../../models";
 import { categoryResourceOptions } from "./category";
-import { courseResourceOptions } from "./course";
-import { episodeResourceOptions } from "./episode";
+import { courseResourceFeatures, courseResourceOptions } from "./course";
+import { episodeResourceFeatures, episodeResourceOptions } from "./episode";
 
 export const adminJsResources: ResourceWithOptions[] = [
   {
     resource: Course,
     options: courseResourceOptions,
+    features: courseResourceFeatures,
   },
   {
     resource: Episode,
     options: episodeResourceOptions,
+    features: episodeResourceFeatures,
   },
   {
     resource: Category,
