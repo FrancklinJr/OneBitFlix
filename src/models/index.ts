a1461489507682e95c41bd8b30aac288ce6307ed
Francklin <francklinjunior@outlook.com>
2023-07-16 20:26:28 -0300

    Tabela de Episódios

--- src/models/index.ts
+++ src/models/index.ts
@@ -2,9 +2,13 @@
 
 import { Category } from "./category";
 import { Course } from "./course";
+import { Episode } from "./episode";
 
 Category.hasMany(Course);
 
 Course.belongsTo(Category);
+Course.hasMany(Episode);
 
-export { Course, Category };
+Episode.belongsTo(Course);
+
+export { Course, Category, Episode };
